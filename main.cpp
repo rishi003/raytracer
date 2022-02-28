cc8dcec6d3365d70a4ec567ffa1aa9b8dacd37c6
user <rishabh@nuevezo.com>
2022-02-28 11:11:19 +0400

    Vector and color utilities completed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include "./custom_headers/vec3.h"
+#include "./custom_headers/color.h"
 
 int main()
 {
-    puts("Hello world@");
+    // Image
+    const int image_width = 256;
+    const int image_height = 256;
+
+    // Image file
+    std::ofstream image_file;
+    image_file.open("image.ppm");
+
+    // Render
+    image_file << "P3\n"
+               << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            color pixel_color(double(i) / (image_width), double(j) / (image_height), 0.25);
+            write_color(image_file, pixel_color);
+        }
+    }
+    image_file.close();
 }
