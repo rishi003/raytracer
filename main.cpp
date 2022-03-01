5ce237dda0bcb6349557131b81eaf02d78d0112a
user <rishabh@nuevezo.com>
2022-03-01 11:38:49 +0400

    linear gradient using rays

--- main.cpp
+++ main.cpp
@@ -2,16 +2,35 @@
 #include <fstream>
 #include "./custom_headers/vec3.h"
 #include "./custom_headers/color.h"
+#include "./custom_headers/ray.h"
+
+color ray_color(const ray &r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 0.2);
+}
 
 int main()
 {
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Image file
     std::ofstream image_file;
     image_file.open("image.ppm");
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * aspect_ratio;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
     image_file << "P3\n"
@@ -21,7 +40,10 @@ int main()
     {
         for (int i = 0; i < image_width; ++i)
         {
-            color pixel_color(double(i) / (image_width), double(j) / (image_height), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(image_file, pixel_color);
         }
     }
